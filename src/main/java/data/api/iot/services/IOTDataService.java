f48f827c8642f1fff3e16c8a93f5e00b0e7af96d
Umer Farooq <umercis@gmail.com>
2018-11-01 15:04:59 +0500

    Update configuration

--- src/main/java/data/api/iot/services/IOTDataService.java
+++ src/main/java/data/api/iot/services/IOTDataService.java
@@ -1,13 +1,13 @@
 package data.api.iot.services;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.stereotype.Service;
 import data.api.entities.Rule;
 import data.mongo.reader.MongoDBReader;
@@ -17,20 +17,20 @@ public class IOTDataService {
    
 	private List<Rule> rules;
 	
+	
+	@Autowired
+	private MongoProperties configuration;
+	MongoDBReader mongoDBReader = new MongoDBReader();
 	private static final Logger IOTDataServiceLogger = LogManager.getLogger(IOTDataService.class);
 	
 	public IOTDataService() {
-		this.rules = new ArrayList<Rule>();
-		//rules.add(new Rule(new ObjectId("569ed8269353e9f4c51617aa"),"testRuleGroupName1","testRuleName1","RULE","Lorem","ENCODE_TAG_TEMP * 40 > 1000",null,"Umer",new Date(),"Umer",new Date(),"N",new Date()));
-		//rules.add(new Rule(new ObjectId("569ed8269353e9f4c51617ab"),"testRuleGroupName1","testRuleName2","RULE","Lorem","ENCODE_TAG_TEMP * 40 > 1000",null,"Umer",new Date(),"Umer",new Date(),"N",new Date()));
-		//rules.add(new Rule(new ObjectId("569ed8269353e9f4c51617ac"),"testRuleGroupName2","testRuleName3","RULE","Lorem","ENCODE_TAG_TEMP * 40 > 1000",null,"Umer",new Date(),"Umer",new Date(),"N",new Date()));
+		
 	}
 	
 	public List<Rule> getIOTData(){
-		MongoDBReader mongoDBReader = new MongoDBReader();
 		List<Rule> rules = new ArrayList<Rule>();
 		try {
-			mongoDBReader.prepareConnection("mongodb://localhost:27017", "rulesengine", "test", "test",IOTDataServiceLogger);
+			mongoDBReader.prepareConnection(configuration.getUri(),configuration.getDatabase(),configuration.getUsername(), configuration.getPassword().toString(),IOTDataServiceLogger);
 			rules = mongoDBReader.getIOTData();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -42,10 +42,9 @@ public class IOTDataService {
 	}
 	
 	public List<Rule> getRules(String ruleGroupName) {
-		MongoDBReader mongoDBReader = new MongoDBReader();
 		List<Rule> rules = new ArrayList<Rule>();
 		try {
-			mongoDBReader.prepareConnection("mongodb://localhost:27017", "rulesengine", "test", "test",IOTDataServiceLogger);
+			mongoDBReader.prepareConnection(configuration.getUri(),configuration.getDatabase(),configuration.getUsername(), configuration.getPassword().toString(),IOTDataServiceLogger);
 			rules = mongoDBReader.getRules(ruleGroupName);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -54,24 +53,19 @@ public class IOTDataService {
 		
 		return rules;
 		
-		
-		//return this.rules.stream().filter(x -> x.getRuleGroupName().equalsIgnoreCase(ruleGroupName)).collect(Collectors.toList());
 	}
 	
 	public Rule getRule(String ruleGroupName,String ruleName) {
-		MongoDBReader mongoDBReader = new MongoDBReader();
 		Rule rule = new Rule();
 		try {
-			mongoDBReader.prepareConnection("mongodb://localhost:27017", "rulesengine", "test", "test",IOTDataServiceLogger);
-			rule = mongoDBReader.getRule(ruleGroupName,ruleName);
+			mongoDBReader.prepareConnection(configuration.getUri(),configuration.getDatabase(),configuration.getUsername(), configuration.getPassword().toString(),IOTDataServiceLogger);
+            rule = mongoDBReader.getRule(ruleGroupName,ruleName);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		return rule;
-		
-		//return this.rules.stream().filter(x -> x.getRuleGroupName().equalsIgnoreCase(ruleGroupName) && x.getRuleName().equalsIgnoreCase(ruleName)).findFirst().get();
+		return rule;	
 	}
 	
 	public boolean evaluateRule(String ruleGroupName,String ruleName,Map<String,String> inputMap) {
--- src/main/java/data/mongo/config/MongoConfig.java
+++ src/main/java/data/mongo/config/MongoConfig.java
@@ -3,6 +3,7 @@ package data.mongo.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @PropertySource("classpath:application.properties")
